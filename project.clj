4518b66e6a7cdf05212b2d4fe67564d060433968
Bruce Hauman <bhauman@gmail.com>
2018-06-27 14:31:18 -0400

    bump to 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject com.bhauman/figwheel-repl "0.1.3"
+(defproject com.bhauman/figwheel-repl "0.1.4-SNAPSHOT"
   :description  "Figwheel REPL provides a stable multiplexing REPL for ClojureScript"
+  :url "https://github.com/bhauman/figwheel-repl"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [ring/ring-core "1.6.3"]
@@ -8,6 +9,9 @@
                  [co.deps/ring-etag-middleware "0.2.0"]
                  [ring-cors "0.1.12"]]
 
+  :scm {:name "git"
+        :url "https://github.com/bhauman/figwheel-repl"}
+  
   ;; for figwheel jetty server - these should probably
   :profiles {:dev {:dependencies [[ring "1.6.3"]
                                   [org.eclipse.jetty.websocket/websocket-servlet "9.2.21.v20170120"]
