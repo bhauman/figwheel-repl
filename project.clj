26de799f85da766c4f266d2235b58cec4c96db08
Bruce Hauman <bhauman@gmail.com>
2018-09-10 20:59:35 -0400

    bump ring version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.bhauman/figwheel-repl "0.1.9"
+(defproject com.bhauman/figwheel-repl "0.2.0-SNAPSHOT"
   :description  "Figwheel REPL provides a stable multiplexing REPL for ClojureScript"
   :url "https://github.com/bhauman/figwheel-repl"
   :license {:name "Eclipse Public License - v 1.0"
@@ -6,20 +6,18 @@
   :scm {:name "git"
         :url "https://github.com/bhauman/figwheel-repl"}
   
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.10.238"]
-                 [ring/ring-core "1.6.3"]
-                 [ring/ring-defaults "0.3.1"]
-                 [ring/ring-devel "1.6.3"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.339"]
+                 [ring/ring-core "1.7.0"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-devel "1.7.0"]
                  [co.deps/ring-etag-middleware "0.2.0"]
                  [ring-cors "0.1.12"]]
-
-
   
   ;; for figwheel jetty server - these should probably
-  :profiles {:dev {:dependencies [[ring "1.6.3"]
-                                  [org.eclipse.jetty.websocket/websocket-servlet "9.2.21.v20170120"]
-                                  [org.eclipse.jetty.websocket/websocket-server "9.2.21.v20170120"]]
+  :profiles {:dev {:dependencies [[ring "1.7.0"]
+                                  [org.eclipse.jetty.websocket/websocket-servlet "9.2.24.v20180105"]
+                                  [org.eclipse.jetty.websocket/websocket-server "9.2.24.v20180105"]]
                    :resource-paths ["resources" "dev-resources"]}
              }
   )
