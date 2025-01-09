85656d65dc5d983556625713e623d2ac39da1eb7
Bruce Hauman <bhauman@gmail.com>
2025-01-09 15:28:38 -0600

    update deps

--- project.clj
+++ project.clj
@@ -12,14 +12,11 @@
                  [ring/ring-defaults "0.5.0"]
                  [ring/ring-devel "1.13.0"]
                  [ring/ring-jetty-adapter "1.13.0"]
-                 [org.ring-clojure/ring-jakarta-servlet "1.13.0"]
                  [co.deps/ring-etag-middleware "0.2.1"]
                  [ring-cors "0.1.13"]]
 
   ;; for figwheel jetty server - these should probably
-  :profiles {:dev {:dependencies [[ring "1.13.0"]
-                                  [org.eclipse.jetty.websocket/websocket-servlet "11.0.24"]
-                                  [org.eclipse.jetty.websocket/websocket-jetty-server "11.0.24"]]
+  :profiles {:dev {:dependencies [[ring "1.13.0"]]
                    :source-paths ["src"]
                    :resource-paths ["resources" "dev-resources"]}
              }
