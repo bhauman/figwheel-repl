db18e3f964aea930ce2f5b47cce08a0fff672c5f
Bruce Hauman <bhauman@gmail.com>
2025-01-09 14:30:15 -0600

    fixing up deps

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject com.bhauman/figwheel-repl "0.2.19-JETTY11"
+(defproject com.bhauman/figwheel-repl "0.2.19-SNAPSHOT"
   :description  "Figwheel REPL provides a stable multiplexing REPL for ClojureScript"
   :url "https://github.com/bhauman/figwheel-repl"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/bhauman/figwheel-repl"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.773" :exclusions [commons-codec]]
-                 [org.clojure/data.json "2.5.1"]
+                 [org.clojure/data.json "2.4.0"]
                  [ring/ring-core "1.13.0"]
                  [ring/ring-defaults "0.5.0"]
                  [ring/ring-devel "1.13.0"]
